c41f831c158f220ec4b41a12c7a0ae33fa6d2b3d
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2022-09-29 21:42:38 +0900

    [fix]add repr C

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,15 +1,21 @@
 #![no_std]
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(C)]
 pub struct PhysAddr(u64);
 
 impl PhysAddr {
     pub fn new(pa: u64) -> Self {
         Self(pa)
     }
+
+    pub fn to_u64(&self) -> u64 {
+        self.0
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(C)]
 pub struct VirtAddr(u64);
 
 impl VirtAddr {
