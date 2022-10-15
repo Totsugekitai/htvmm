622a25bac58a7ee22a19b47fb5ff3f8073f2c04f
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2022-10-16 05:48:05 +0900

    [wip]virt

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -1,11 +1,17 @@
 #![no_std]
 
+mod arch;
+mod cpu;
+mod virt;
+
+use crate::arch::intel::IntelCpu;
 use common::BootArgs;
 use core::{
     arch::{asm, global_asm},
     panic::PanicInfo,
     ptr,
 };
+use cpu::Cpu;
 
 global_asm!(include_str!("entry.s"), options(att_syntax));
 
@@ -13,9 +19,13 @@ global_asm!(include_str!("entry.s"), options(att_syntax));
 pub unsafe extern "C" fn vmm_main(boot_args: *const BootArgs) {
     let _boot_args = (&*boot_args).clone();
     clear_bss();
-    // loop {
-    //     asm!("hlt");
-    // }
+    let mut intel = IntelCpu::new();
+    if intel.is_virtualization_supported() {
+        for _ in 0..10000000 {
+            asm!("nop");
+        }
+        let _ = intel.enable_virtualization();
+    }
 }
 
 #[panic_handler]
