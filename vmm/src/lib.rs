6a1c7a871b394758c394de40c4dc17538cb99f47
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2022-10-16 12:03:58 +0900

    [fix]little fix

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -1,11 +1,16 @@
 #![no_std]
+#![feature(default_alloc_error_handler)]
 
+mod allocator;
 mod arch;
 mod cpu;
 mod virt;
 
+extern crate alloc;
+
 use crate::arch::intel::IntelCpu;
-use common::BootArgs;
+use alloc::vec::Vec;
+use common::{BootArgs, VMM_AREA_HEAD_VADDR, VMM_AREA_SIZE, VMM_HEAP_HEAD_VADDR};
 use core::{
     arch::{asm, global_asm},
     panic::PanicInfo,
@@ -13,18 +18,29 @@ use core::{
 };
 use cpu::Cpu;
 
+extern "C" {
+    static __bss: u8;
+    static __bss_end: u8;
+}
+
 global_asm!(include_str!("entry.s"), options(att_syntax));
 
 #[no_mangle]
 pub unsafe extern "C" fn vmm_main(boot_args: *const BootArgs) {
     let _boot_args = (&*boot_args).clone();
+    allocator::init(
+        VMM_HEAP_HEAD_VADDR,
+        VMM_AREA_SIZE as usize - (VMM_HEAP_HEAD_VADDR - VMM_AREA_HEAD_VADDR),
+    );
     clear_bss();
     let mut intel = IntelCpu::new();
+    let mut v = Vec::new();
     if intel.is_virtualization_supported() {
-        for _ in 0..10000000 {
+        for i in 0..10000 {
             asm!("nop");
+            v.push(i);
         }
-        let _ = intel.enable_virtualization();
+        // let _ = intel.enable_virtualization();
     }
 }
 
@@ -35,11 +51,6 @@ fn panic(_: &PanicInfo) -> ! {
             asm!("hlt");
         }
     }
-}
-
-extern "C" {
-    static __bss: u8;
-    static __bss_end: u8;
 }
 
 unsafe fn clear_bss() {
