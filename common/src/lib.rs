8f5ea905dd42a0d7b58cfbb597feba1e23536b24
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2022-10-16 12:01:48 +0900

    [update]add constant value

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,6 +2,10 @@
 
 use x86_64::{registers::control::Cr3Flags, PhysAddr};
 
+pub const VMM_AREA_SIZE: u64 = 512 * 1024 * 1024;
+pub const VMM_AREA_HEAD_VADDR: usize = 0x1_0000_0000;
+pub const VMM_HEAP_HEAD_VADDR: usize = VMM_AREA_HEAD_VADDR + (128 * 1024 * 1024);
+
 #[derive(Debug, Clone)]
 #[repr(C)]
 pub struct BootArgs {
