c3564388cbb2792471892b0c61876ec2750b9e18
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2022-10-19 22:31:32 +0900

    [update]use linked_list_allocator

--- vmm/src/allocator.rs
+++ vmm/src/allocator.rs
@@ -3,6 +3,6 @@ use linked_list_allocator::LockedHeap;
 #[global_allocator]
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
-pub unsafe fn init(head: usize, len: usize) {
-    ALLOCATOR.lock().init(head, len);
+pub unsafe fn init(heap: usize, len: usize) {
+    ALLOCATOR.lock().init(heap as *mut u8, len);
 }
